f6efdf2a2bc9c7d9cad2c1ac856b28e2dbe604da
qwertyu <luke.z24680@gmail.com>
2026-01-09 08:41:27 -0500

    first working iteration with email sending, also will use jakarta for just imap

--- src/main/java/com/luke/jmailer/App.java
+++ src/main/java/com/luke/jmailer/App.java
@@ -1,37 +1,86 @@
 package com.luke.jmailer;
 
-import java.util.Scanner;
-
-import org.simplejavamail.api.email.Email;
-import org.simplejavamail.api.mailer.config.TransportStrategy;
-import org.simplejavamail.email.EmailBuilder;
-import org.simplejavamail.mailer.MailerBuilder;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import org.yaml.snakeyaml.Yaml;
 
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("JMAILER TEST - COCK.LI");
-        System.out.println("email: ");
-        String username = scan.nextLine();
-        System.out.println("password: ");
-        String password = scan.nextLine();
-        scan.close();
-        
-        Email email = EmailBuilder.startingBlank()
-            .from("Testing", username)
-            .to("qwertyu", "")
-            .withSubject("Testing from JMail v0.0.1")
-            .withPlainText("testing")
-            .buildEmail();
+        String configPath = System.getProperty("user.home") + "/.jmailer.d/config.yaml";
+        System.out.println(configPath);
 
-        MailerBuilder
-            .withSMTPServer("mail.cock.li", 587, username, password)
-            .withTransportStrategy(TransportStrategy.SMTP_TLS)
-            .withDebugLogging(true)
-            .buildMailer()
-            .sendMail(email);
+        Sender sender = loadSender(configPath);
+        sender.sendMail("testing", "Testing from JMail V0.0.2", "john.s12312355@gmail.com");
+
     }
+
+    // sets up sender with configuration properties
+    // i probably could have put this just in the sender class
+    public static Sender loadSender(String configPath) {
+        Yaml yaml = new Yaml();
+        try {
+            InputStream inputStream = new FileInputStream(configPath);
+            Config config = yaml.loadAs(
+                inputStream,
+                Config.class
+            );
+            // by default, if no username specified, set it to email
+            config.SMTP.checkUsername();
+            config.IMAP.checkUsername();
+
+            // p stands for properties to keep things shorter
+            SMTPConfig p = config.SMTP;
+
+            return new Sender(p.server, p.email, p.username, p.password, p.port);
+        } catch (FileNotFoundException e) {
+            // add config wizard
+            System.out.println("config not found");
+            return new Sender("a", "a", "a", "a", 2);
+        }
+    }
+
+    // make this later
+    //public static void configurationWizard
+}
+
+// map the config to these classes
+// config will be formatted as
+// config
+// +- SMTP details
+// \- IMAP details
+// see: App.loadSender as well as the IMAPConfig and Config classes
+class SMTPConfig {
+    public String server;
+    public int port;
+    public String email;
+    public String username;
+    public String password;
+
+    public void checkUsername() {
+        if (this.username == null) {
+            this.username = this.email;
+        }
+    }
+}
+
+class IMAPConfig {
+    public String server;
+    public int port;
+    public String email;
+    public String username;
+    public String password;
+    public void checkUsername() {
+        if (this.username == null) {
+            this.username = this.email;
+        }
+    }
+}
+
+class Config {
+    public SMTPConfig SMTP;
+    public IMAPConfig IMAP;
 }
--- src/main/java/com/luke/jmailer/Sender.java
+++ src/main/java/com/luke/jmailer/Sender.java
@@ -1,6 +1,52 @@
 package com.luke.jmailer;
 
-// autofill parts of an email based on configuration files and
-// create the mailer
+import org.simplejavamail.api.email.Email;
+import org.simplejavamail.api.mailer.Mailer;
+import org.simplejavamail.api.mailer.config.TransportStrategy;
+import org.simplejavamail.email.EmailBuilder;
+import org.simplejavamail.mailer.MailerBuilder;
+
+/* Acts as the class that sends emails, email bodies are composed from 
+ * the main App class.
+ */
+
+// mailer wrapper that autofills information on emails
 public class Sender {
+    private final Mailer mailer;
+    private final String server;
+    private final String emailAddr;
+    private final String username;
+    private final String password;
+    private final int port;
+
+    // if username is provided
+    Sender(String server, String emailAddr, String username, String password, int port) {
+        this.server = server;
+        this.emailAddr = emailAddr;
+        this.username = username;
+        this.password = password;
+        this.port = port;
+
+        // remember to enable tls if port is 587
+        this.mailer = MailerBuilder
+            .withSMTPServer(this.server, this.port, this.username, this.password)
+            .withTransportStrategy(TransportStrategy.SMTP_TLS)
+            .buildMailer();
+    }
+
+    public void sendMail(String body, String subject, String recipient) {
+        Email email = EmailBuilder.startingBlank()
+            .from(username, emailAddr)
+            .to(recipient)
+            .withSubject(subject)
+            .withPlainText(body)
+            .buildEmail();
+        mailer.sendMail(email);
+    }
+
+    // UNUSED CODE
+    // if no username is specified (check is done in function call)
+    //Sender(String server, String emailAddr, String password, int port) {
+    //    this(server, emailAddr, emailAddr, password, port);
+    //}
 }
